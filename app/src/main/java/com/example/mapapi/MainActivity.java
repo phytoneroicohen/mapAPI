5d8d02358f376b2fbe4ccad88df98d49583cdae6
roie <roiehagever2006@gmail.com>
2024-12-21 18:02:18 +0200

    firt

---
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mapapi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    //private GoogleMap gmap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        LatLng Ash = new LatLng(31.6767, 34.5762);
+        googleMap.addMarker(new MarkerOptions()
+                .position(Ash)
+                .title("Marker in Ashkelon"));
+    }
+}
